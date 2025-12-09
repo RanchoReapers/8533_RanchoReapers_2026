31c4325d1e6c1fafcf9ad03bbb57e302e2dd078e
Nathaniel <nexnotone@proton.me>
2025-12-09 10:18:16 -0800

    replaced SwerveModule FeedbackSensor import: ->  com.revrobotics.spark.FeedbackSensor; UNTESTED REPLACEMENT -- com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor appears deprecated, this seems to stop throwing errors

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -5,10 +5,10 @@ import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.signals.SensorDirectionValue;
 import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.FeedbackSensor; // UNTESTED REPLACEMENT -- com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor appears deprecated, this seems to stop throwing errors
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
