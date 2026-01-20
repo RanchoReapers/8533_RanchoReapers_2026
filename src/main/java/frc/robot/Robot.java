61c9dcbd177f234ca062f1eabe7a1872258589da
Nathaniel <nexnotone@proton.me>
2026-01-20 11:34:18 -0800

    commented out auto logic; fixing

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -47,7 +47,7 @@ public class Robot extends TimedRobot {
 
     @Override
     public void autonomousInit() {
-        m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+       //  m_autonomousCommand = m_robotContainer.getAutonomousCommand();
         // schedule the autonomous command 
         if (m_autonomousCommand != null) {
             CommandScheduler.getInstance().schedule(m_autonomousCommand);
